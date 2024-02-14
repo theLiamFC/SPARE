5d82789cea783677e0d1904a65b32d5d5b4f4dd3
theLiamFC <147665958+theLiamFC@users.noreply.github.com>
2024-02-14 11:09:48 -0500

    creating a new file

---
+++ openAIAlchemy.py
@@ -0,0 +1,20 @@
+from openai import OpenAI
+import time
+
+
+class openAIAlchemy:
+    def __init__():
+        client = OpenAI()
+
+    # Retreive assistants for purpose of finding IDs
+    def getAssistants(self):
+        my_assistants = self.client.beta.assistants.list(
+            order="desc",
+            limit="20",
+        )
+        return my_assistants.data
+
+    # Retrieve runs in a given thread
+    def getRuns(self, thread_id):
+        runs = self.client.beta.threads.runs.list(thread_id)
+        return runs
