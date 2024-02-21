582290797fb8647de68a6ecbcf399e278a8ca873
theLiamFC <147665958+theLiamFC@users.noreply.github.com>
2024-02-21 18:22:41 -0500

    testing

--- openAIAlchemy.py
+++ openAIAlchemy.py
@@ -17,13 +17,15 @@ import serial_interface
 # - build out interface loop to communicate over multiple runs
 # - integrate json file for automated documentation returns
 # - expand json file with more SPIKE syntax
+# - improve commenting and readability
+
 
 class openAIAlchemy:
     def __init__(self, assistant_id, thread_id=None, debug=False):
         self.client = OpenAI()
         self.debug = debug
         self.run_id = None
-        #self.debugText # build out logging functionality and export to txt file
+        # self.debugText # build out logging functionality and export to txt file
 
         if thread_id == None:
             newThread = self.client.beta.threads.create()
@@ -84,27 +86,31 @@ class openAIAlchemy:
 
     async def __runManager(self):
         if self.run_id == None:
-            if self.debug: print("Creating new run")
+            if self.debug:
+                print("Creating new run")
             run = self.client.beta.threads.runs.create(
                 thread_id=self.thread_id, assistant_id=self.assistant_id
             )
             self.run_id = run.id
         else:
-            if self.debug: print("Using existing run")
-        if self.debug: print("Run in progress")
+            if self.debug:
+                print("Using existing run")
+        if self.debug:
+            print("Run in progress")
         status = "in_progress"
-        
+
         # non asyncio version
         lastTime = time.time()
-        while status not in ["completed", "failed","requires_action"]:
+        while status not in ["completed", "failed", "requires_action"]:
             status = self.client.beta.threads.runs.retrieve(
                 thread_id=self.thread_id, run_id=self.run_id
             ).status
             if self.debug and time.time() - lastTime > 5:
-                print("Longer than normal runtime: ",status)
+                print("Longer than normal runtime: ", status)
                 lastTime = time.time()
             await asyncio.sleep(1)
-        if self.debug: print("Status: " + status)
+        if self.debug:
+            print("Status: " + status)
         if status == "requires_action":
             calls = self.client.beta.threads.runs.retrieve(
                 thread_id=self.thread_id, run_id=self.run_id
@@ -114,7 +120,12 @@ class openAIAlchemy:
         elif status == "completed":
             # Assuming run_details contains a 'result' attribute with the data you need
             self.run_id = None
-            return self.client.beta.threads.messages.list(self.thread_id).data[0].content[0].text.value  # Return the actual result here
+            return (
+                self.client.beta.threads.messages.list(self.thread_id)
+                .data[0]
+                .content[0]
+                .text.value
+            )  # Return the actual result here
         elif status == "failed":
             # throw exception
             self.run_id = None
@@ -122,7 +133,8 @@ class openAIAlchemy:
             print(
                 self.client.beta.threads.runs.retrieve(
                     thread_id=self.thread_id, run_id=run.id
-                ))
+                )
+            )
 
     def print_break(self, name, body):
         print("==================== " + name + " ====================")
@@ -130,7 +142,8 @@ class openAIAlchemy:
         print("==================== " + "end" + " ====================")
 
     def __functionManager(self, calls):
-        if self.debug: print("Managing functions")
+        if self.debug:
+            print("Managing functions")
         tool_outputs = []
 
         for toolCall in calls.submit_tool_outputs.tool_calls:
@@ -142,51 +155,44 @@ class openAIAlchemy:
             # case for every function available to assistant
             if name == "get_feedback":
                 # print arg to command line and get written feedback
-                print("Hey Human: ",args['prompt'])
+                print("Hey Human: ", args["prompt"])
                 human_response = input()
-                tool_outputs.append({
-                    "tool_call_id": id,
-                    "output": human_response,
-                })
+                tool_outputs.append(
+                    {
+                        "tool_call_id": id,
+                        "output": human_response,
+                    }
+                )
             elif name == "get_documentation":
-                print("Query Dict: ",args['query'])
+                print("Query Dict: ", args["query"])
                 query_response = input()
-                tool_outputs.append({
-                    "tool_call_id":id,
-                    "output":query_response
-                }) 
+                tool_outputs.append({"tool_call_id": id, "output": query_response})
             elif name == "run_code":
-                code = args['code']
+                code = args["code"]
                 self.print_break("CODE", code)
                 code = code.replace("\n", "\r\n")
 
-                serial_response = serial_interface.serial_write(bytes(code, 'utf-8'))
-                
-                if self.debug: 
+                serial_response = serial_interface.serial_write(bytes(code, "utf-8"))
+
+                if self.debug:
                     self.print_break("SERIAL OUTPUT", serial_response)
-                tool_outputs.append({
-                    "tool_call_id":id,
-                    "output":serial_response
-                }) 
+                tool_outputs.append({"tool_call_id": id, "output": serial_response})
 
         self.client.beta.threads.runs.submit_tool_outputs(
-            thread_id = self.thread_id,
-            run_id=self.run_id,
-            tool_outputs=tool_outputs
+            thread_id=self.thread_id, run_id=self.run_id, tool_outputs=tool_outputs
         )
 
-
-    def extract_code(self,  result):
+    def extract_code(self, result):
         idx1 = result.find("```") + 3 + 7
         idx2 = result[idx1:].find("```") + idx1
         code = result[idx1:idx2]
-        breaker  = "============ CODE ============\n"
+        breaker = "============ CODE ============\n"
         breaker1 = "==============================\n"
-        response = result[:idx1-10] + breaker + code + breaker1 + result[idx2+3:]
-        
+        response = result[: idx1 - 10] + breaker + code + breaker1 + result[idx2 + 3 :]
+
         return (code, response)
-    
-    def serialTalk(self,code):
+
+    def serialTalk(self, code):
         pass
 
     # Public method to start the OpenAI run asynchronously
